34e5ba245a1f7b88f91058a7301ac91b975eda80
iltsy26 <71667539+iltsy26@users.noreply.github.com>
2022-03-11 11:57:51 +0300

    Add files via upload

---
+++ LAB_1_High-speed_Comp/LAB_1_High-speed_Comp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LAB_1_High_speed_Comp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Func<Action<string, int>, int, int> lambda = Func1;
+            Action<string, int> actMeth = Act1;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                lambda(actMeth, i);
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+
+        private static void Act1(string arg1, int arg2)
+        {
+            Console.Write(arg1);
+            Console.Write(arg2);
+
+        }
+
+        private static int Func1(Action<string, int> arg1, int arg2)
+        {
+            int res = arg2 * arg2;
+            arg1("The oroginal number: ", arg2);
+            arg1("\t The square of the number: ", res);
+            return (res);
+        }
+    }
+}
